fe3a524643cb18688b30495fc8d6c5c4288aca32
dmdoa12 <dmdoa12@gmail.com>
2016-11-11 19:30:43 +0900

    try again

--- app/src/main/java/com/example/a403/exercise/MainActivity.java
+++ app/src/main/java/com/example/a403/exercise/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.a403.exercise;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    TextView t1;
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "안녕????", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        t1 = (TextView)findViewById(R.id.text1);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Hello World!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "try again", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
